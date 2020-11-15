0f51fe3c5b1f58ae2c95af6d2b669af7e64832cc
Francisco Villar Salvador <franvlive@gmail.com>
2020-11-15 10:58:04 +0100

    Image - First design version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./app.scss";
+import Image from "./components/image";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Image />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/image/image.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./image.scss";
+
+const Image = () => {
+  return (
+    <section className="image">
+      <div className="image__info">
+        <img
+          class="image__logo"
+          src="https://cdn.worldvectorlogo.com/logos/amazon-2.svg"
+          alt="Amazon"
+        />
+        <p className="image__slogan">
+          Our vision is to be earth's most customer-centric company; to build a
+          place where people can come to find and discover anything they might
+          want to buy online.
+        </p>
+        <button className="image__button">Register</button>
+      </div>
+    </section>
+  );
+};
+
+export default Image;
